54b00f1b299a464e20f4e7d3764e3046b782507c
MARCOS EDUARDO DA SILVA SANTOS <marcoseduardoss@gmail.com>
2023-09-15 21:16:50 -0300

    Update script.sql

--- Exemplo3ArquieturaTresCamadas/script.sql
+++ Exemplo3ArquieturaTresCamadas/script.sql
@@ -1,5 +1,13 @@
-CREATE TABLE Tarefa (
+-- Criação da tabela "Tarefa" para armazenar informações sobre tarefas
+CREATE TABLE IF NOT EXISTS Tarefa (
+    
+    -- Coluna "id" que será a chave primária autoincrementada (sempre soma 1 a cada registro incluído)
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    
+    -- Coluna "titulo" para armazenar o título da tarefa (não pode ser nulo)
     titulo TEXT NOT NULL,
+    
+    -- Coluna "descricao" para armazenar a descrição da tarefa (não pode ser nulo)
     descricao TEXT NOT NULL
 );
+
